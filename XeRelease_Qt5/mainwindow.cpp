53b0bad898a9e8df32eae1e92071089277b68720
daleclack <daleclack1@foxmail.com>
2023-07-05 15:28:24 +0800

    Add background

--- XeRelease_Qt5/mainwindow.cpp
+++ XeRelease_Qt5/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setStyleSheet("QMainWindow {border-image:url(:/xe_res/res/fly.png)}");
 }
 
 MainWindow::~MainWindow()
